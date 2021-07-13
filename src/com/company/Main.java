b55e14059dc7b3bc82710ba1c4de7e074ebb87fb
boronbai <boronbai.kasimov@gmail.com>
2021-07-13 17:35:45 +0600

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,28 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int [] arr = new int[6];
+        arr[0] = 1;
+        arr[1] = 2;
+        arr[2] = 3;
+        arr[3] = 4;
+        arr[4] = 5;
+        arr[5] = 6;
+
+        int jupSan = 2 ;
+
+        for (int i = 0; i<arr.length;i++) {
+            if ( arr[i] % jupSan == 0 ) {
+                //jupSan++;
+                System.out.println(arr[i] + " " + i);
+            }
+
+        }
+
+    }
+}
